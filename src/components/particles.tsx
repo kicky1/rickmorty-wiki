c3b22d8b6404333fbd86a10dbf6700c3a3a22eea
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-09-18 20:38:23 +0200

    fix: types

--- src/components/particles.tsx
+++ src/components/particles.tsx
@@ -21,6 +21,7 @@ export default function Particles({
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const canvasContainerRef = useRef<HTMLDivElement>(null);
 	const context = useRef<CanvasRenderingContext2D | null>(null);
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const circles = useRef<any[]>([]);
 	const mousePosition = useMousePosition();
 	const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
